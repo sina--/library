c9e1dce1341165f4250cf75466b4566b025e9d00
Sina <sina@thewolpers.com>
2022-12-16 12:13:28 -0800

    Create library HTML elements from array

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let testLibrary = [
 	{
-		'title' = 'Animal Farm',
-		'author' = 'George Orwell',
-		'published' = '1945',
-		'id' = '0',
-		'status' = 'unread',
+		'title' : 'Animal Farm',
+		'author' : 'George Orwell',
+		'published' : '1945',
+		'id' : '0000',
+		'status' : 'unread',
 	},
 ];
 
 let myLibrary = [];
 
+let shelf = document.getElementById('library');
+
 function Book() {
 }
 
-function addBookToLibrary() {
+function init() {
+	for (i = 0; i < testLibrary.length; i++) {
+		book = document.createElement('div');
+		book.setAttribute('id', testLibrary[i].id);
+		book.setAttribute('class', 'book-card');
+		shelf.appendChild(book);
+	}
 }
+
+init();
