b2f9a6af376b1f43fe774cbe28efb336f305fb99
Sina <sina@thewolpers.com>
2022-12-16 11:58:28 -0800

    Create basic script file

---
+++ script.js
@@ -0,0 +1,17 @@
+let testLibrary = [
+	{
+		'title' = 'Animal Farm',
+		'author' = 'George Orwell',
+		'published' = '1945',
+		'id' = '0',
+		'status' = 'unread',
+	},
+];
+
+let myLibrary = [];
+
+function Book() {
+}
+
+function addBookToLibrary() {
+}
