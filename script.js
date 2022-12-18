ff4ca4b43b00f5b1aeba7dfa9faa1a1e14b9981a
Sina Wolper <sina@thewolpers.com>
2022-12-17 17:14:34 -0800

    Add author and year of publication

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let testLibrary = [
 	{
-		'title' : 'Animal Farm',
-		'author' : 'George Orwell',
-		'published' : '1945',
-		'id' : '0000',
-		'status' : 'unread',
+		title : 'Animal Farm',
+		author : 'George Orwell',
+		published : '1945',
+		id : '0000',
+		state : 'unread',
 	},
 ];
 
@@ -20,10 +20,19 @@ function init() {
 		book.setAttribute('id', testLibrary[i].id);
 		book.setAttribute('class', 'book-card');
 		let bookTitle = document.createElement('h1');
+		let details = document.createElement('div');
+		details.setAttribute('class', 'details');
+		let bookAuthor = document.createElement('h2');
+		let bookYear = document.createElement('h2');
 		bookTitle.setAttribute('class', 'book-title');
 		bookTitle.textContent = testLibrary[i].title;
 		shelf.appendChild(book);
+		bookAuthor.textContent = testLibrary[i].author;
+		bookYear.textContent = testLibrary[i].published;
 		book.appendChild(bookTitle);
+		book.appendChild(details);
+		details.appendChild(bookAuthor);
+		details.appendChild(bookYear);
 	}
 }
 
